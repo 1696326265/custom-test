1aa0a74dcbd65dff9eaf26e3badd2164275bf5a3
zhangzy <1696326265@qq.com>
2019-08-09 17:58:39 +0800

    Create session.py

---
+++ augments/session.py
@@ -0,0 +1,26 @@
+from flask import Flask , session
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+
+tot = 0
+
+@app.route('/set')
+def defau():
+	global tot
+	tot += 1
+	session['usn'] = str(tot)
+	return 'setted'
+
+@app.route('/')
+def see():
+	return session.get('usn')
+
+@app.route('/del')
+def delet():
+	session.clear()
+	return 'cleared'
+
+if __name__=='__main__':
+	app.run(host='0.0.0.0')
